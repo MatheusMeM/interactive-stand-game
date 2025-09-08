49bdf4d51b2024af11ac6012c26633975818406f
MatheusMeM <matheusmsmendes@gmail.com>
2025-09-08 09:05:04 -0300

    fix(pins): Changed the config pins to correct relay-led pins mapping

--- config.py
+++ config.py
@@ -3,8 +3,10 @@
 # 1. GPIO Pin Mapping (BCM numbering)
 # These must be filled with the actual BCM pin numbers you will use.
 # Example: BUTTON_PINS = [4, 17, 27, ...]
-BUTTON_PINS = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13]  # 12 pins for arcade buttons
-RELAY_PINS = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]   # 12 pins for relay channels controlling LEDs
+
+BUTTON_PINS = [13, 6, 5, 2, 3, 4, 11, 9, 10, 17, 27, 22]
+RELAY_PINS  = [7, 8, 21, 25, 24, 23, 20, 16, 12, 18, 15, 14]
+
 
 # 2. Game Timings (in seconds)
 INSTRUCTIONS_DURATION = 5.0
